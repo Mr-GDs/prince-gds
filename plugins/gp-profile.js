90c63fe62ae20cf02d0568008be690d31c15ed60
Mr-Ds <158026058+Mr-GDs@users.noreply.github.com>
2026-06-12 11:37:27 +0500

    Update

--- plugins/gp-profile.js
+++ plugins/gp-profile.js
@@ -1,11 +1,10 @@
-
 import { createHash } from 'crypto'
 import PhoneNumber from 'awesome-phonenumber'
 import { xpRange } from '../lib/levelling.js'
 let handler = async (m, { conn, usedPrefix, command}) => {
 
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-if (!(who in global.db.data.users)) throw `✳️ El usuario no se encuntra en mi base de datos`
+if (!(who in global.db.data.users)) throw `✳️ User not found in my database`
 let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
 let user = global.db.data.users[who]
 let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn, genero, prem, coin, bank} = global.db.data.users[who]
@@ -16,18 +15,18 @@ let premG = global.prems.includes(who.split`@`[0]) || prem
 let sn = createHash('md5').update(who).digest('hex')
 
 let str = `
-┌───「 *PERFIL* 」
-▢ *🔖Nombre:* 
+┌───「 *PROFILE* 」
+▢ *🔖Name:* 
    • ${username} ${registered ? '\n   • ' + name + ' ': ''}
    • @${who.replace(/@.+/, '')}
-▢ *📱Numero:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-▢ *🔗Link:* wa.me/${who.split`@`[0]}${registered ? `\n▢ *🎈Edad:* ${age}\n▢ *🧬Genero:* ${genero}` : ''}
+▢ *📱Number:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+▢ *🔗Link:* wa.me/${who.split`@`[0]}${registered ? `\n▢ *🎈Age:* ${age}\n▢ *🧬Gender:* ${genero}` : ''}
 ▢ *🪙Coins:* ${bank.toLocaleString()}
-▢ *💎Diamantes:* ${diamond.toLocaleString()}
-▢ *🆙Nivel:* ${level}
-▢ *⬆️XP:* Total ${exp} (${user.exp - min} / ${xp})\n${math <= 0 ? `Listo para *${usedPrefix}levelup*` : `_*${math}xp*_ Falta para subir de Nivel`}
-▢ *🏆Rango:* ${role}
-▢ *📇Registrado:* ${registered ? '✅': '❎'}
+▢ *💎Diamonds:* ${diamond.toLocaleString()}
+▢ *🆙Level:* ${level}
+▢ *⬆️XP:* Total ${exp} (${user.exp - min} / ${xp})\n${math <= 0 ? `Ready for *${usedPrefix}levelup*` : `_*${math}xp*_ needed to level up`}
+▢ *🏆Rank:* ${role}
+▢ *📇Registered:* ${registered ? '✅': '❎'}
 ▢ *🎟️Premium:* ${premG ? '✅' : '❎'}
 └──────────────`
     conn.sendFile(m.chat, pp, 'perfil.jpg', str, m, false, { mentions: [who] })
